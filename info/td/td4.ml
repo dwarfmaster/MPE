78c7769a4b7cb13f62d271e61f161661e640c2a3
lucas8 <luc.linux@mailoo.org>
2015-12-22 20:24:38 +0100

    finished info td4

--- info/td/td4.ml
+++ info/td/td4.ml
@@ -156,7 +156,38 @@ let poids_minimal p i j =
     done;
     acc.(i).(j);;
 
-(* TODO chemin minimal *)
+let chemins_minimal p i j =
+    let n = vect_length p in
+    let acc = make_matrix n n 0 in
+    let aux = make_matrix n n 0 in
+    let ant = make_matrix n n 0 in
+    for i = 0 to n-1 do
+        for j = 0 to n-1 do
+            acc.(i).(j) <- p.(i).(j);
+            ant.(i).(j) <- i;
+        done;
+    done;
+    for k = 1 to n do
+        for i = 0 to n-1 do
+            for j = 0 to n-1 do
+                aux.(i).(j) <- p.(i).(k) + p.(k).(j);
+            done;
+        done;
+        for i = 0 to n-1 do
+            for j = 0 to n-1 do
+                if acc.(i).(j) < aux.(i).(j) then
+                    acc.(i).(j) <- acc.(i).(j)
+                else begin
+                    acc.(i).(j) <- aux.(i).(j);
+                    ant.(i).(j) <- k;
+                end;
+            done;
+        done;
+    done;
+    let rec rem a b = if b = a then []
+    else b :: rem a ant.(a).(b)
+    in rem i j;;
+
 (* }}} *)
 
 (* {{{ Exercice 3 *)
@@ -172,6 +203,83 @@ let trou_noir adj =
         if v.(!i) then ed := true;
     done;
     not !ed && llen c.(!i) == n - 1;;
+
+let trou_noir_df adj =
+    let n = vect_length adj in
+    let count = make_vect n 0 in
+    let rec parcours i =
+        count.(i) <- count.(i) + 1;
+        if count.(i) = 1 then begin
+            let nxts = ref adj.(i) in
+            while !nxts <> [] do
+                let h::t = !nxts in
+                nxts := t;
+                parcours h;
+            done;
+        end;
+    in for i = 0 to n-1 do
+        if count.(i) = 0 then parcours i
+    done;
+    let tn = ref (-1) in
+    let i = ref 0 in
+    while !tn < 0 && !i < n do
+        if count.(!i) = n-1 && adj.(!i) = [] then tn := !i;
+        i := !i + 1;
+    done;
+    !tn;;
+
+let trou_noir_matrix m =
+    let n = vect_length m in
+    let i = ref 0 in
+    let ed = ref false in
+    (* Boucle exécutée en O(A) (les deux boucle ne sont pas indépendantes) *)
+    while not !ed && !i < n do
+        let j = ref !i in
+        while !j < n && m.(!i).(!j) == 0 do
+            j := !j + 1;
+        done;
+        if !j == n then ed := true
+                   else i := !j;
+    done;
+    if !i == n then (-1) else begin
+        let is = ref true in
+        (* Autre boucle en O(A) *)
+        for k = 0 to n-1 do
+            is := !is && m.(!i).(k) = 0;
+            if k <> !i then is := !is && m.(k).(!i) = 1;
+        done;
+        if !is then !i else (-1)
+    end;;
+
+(* }}} *)
+
+(* {{{ Exercice 4 *)
+
+let rec rev_cat l1 l2 = match l1 with
+| []   -> l2
+| h::t -> rev_cat t (h::l2);;
+
+let cycle_eulerien g =
+    let n = vect_length g in
+    let adj = g in
+    let rec cycle n =
+        let ccl = ref [n] in
+        let rec follow i = if adj.(i) <> [] then begin
+            let h::t = adj.(i) in
+            adj.(i) <- t;
+            if h <> n then begin
+                ccl := h :: !ccl;
+                follow h
+            end;
+        end;
+        in let rec rem l = match l with
+        | []   -> []
+        | h::t -> if adj.(h) = [] then h :: rem t
+                  else let c2 = cycle h in rev_cat c2 (h :: rem t)
+        in follow n; rem !ccl
+    in 0::cycle 0;;
+
+cycle_eulerien [| [1;3]; [0;2]; [3]; [0;1] |];;
 
 (* }}} *)
 
