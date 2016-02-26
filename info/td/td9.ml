bae642f6aedfb58a11747c22a6ceb6f2e04397d1
lucas8 <luc.linux@mailoo.org>
2016-02-26 19:27:28 +0100

    Finished info td9

--- info/td/td9.ml
+++ info/td/td9.ml
@@ -108,18 +108,78 @@ let arbre_pur = N (N (V,N (N (V,V), N (V,V))), V);;
 
 let rec code_brutal pur = match pur with
 | V         -> [0]
-| N (a1,a2) -> 1 :: code_brutal a1 @ code a2;;
+| N (a1,a2) -> 1 :: code_brutal a1 @ code_brutal a2;;
 
 let rec code_acc pur acc = match pur with
 | V         -> 0 :: acc
 | N (a1,a2) -> 1 :: code_acc a1 (code_acc a2 acc);;
+let code pur = code_acc pur [];;
 
 code_acc arbre_pur [];;
 code_brutal arbre_pur;;
+
+let rec decode_rec cd = match cd with
+| []                -> failwith "Invalid code"
+| h :: t when h = 0 -> (V,t)
+| h :: t when h = 1 -> let (a1,t1) = decode_rec t in
+                       let (a2,t2) = decode_rec t1 in
+                       (N (a1,a2), t2);;
+let decode cd = let (a,_) = decode_rec cd in a;;
+
+decode (code arbre_pur);;
 
 (* TODO decode et fin *)
 
 (* }}} *)
 
-(* TODO question 7 et 8 *)
+(* {{{ Exercice 7 *)
+let rec evalflot_rec pur flot = match (pur,flot) with
+| (V,[])         -> failwith "Bad flow"
+| (V,h::t)       -> (Noeud (h,Nil,Nil), t, h)
+| (N (a1,a2), t) -> let (n1, t1, s1) = evalflot_rec a1 t  in
+                    let (n2, t2, s2) = evalflot_rec a2 t1 in
+                    let s = (s1 + s2) mod 4 in
+                    (Noeud (s, n1, n2), t2, s);;
+
+evalflot_rec arbre_pur [1; 2; 3; 4; 5; 6];;
+
+let rec estcompatible arbre = match arbre with
+| Nil             -> true
+| Noeud (v,a1,a2) -> v != 0 && estcompatible a1 && estcompatible a2;;
+
+(* }}} *)
+
+(* {{{ Exercice 8 *)
+let rec cross_join_r e1 e2 ac =
+    let rec rjoin a e acc = match e with
+    | []   -> acc
+    | h::t -> rjoin a t (Noeud (0,a,h) :: acc)
+    in match e1 with
+    | []   -> ac
+    | h::t -> cross_join_r t e2 (rjoin h e2 ac);;
+let cross_join e1 e2 = cross_join_r e1 e2 [];;
+
+let enumerate n =
+    let mem = make_vect (n+1) [] in
+    mem.(0) <- [Nil];
+    for i = 1 to n do
+        for j = 0 to i - 1 do
+            mem.(i) <- cross_join_r mem.(j) mem.(i-j-1) mem.(i);
+        done;
+    done;
+    mem.(n);;
+
+enumerate 3;;
+
+let rec map f a = match a with
+| []   -> []
+| h::t -> f h :: map f t;;
+
+let enumerate_gen n =
+    let toar l = R (0,l)
+    in map toar (map bin_to_foret (enumerate (n-1)));;
+
+enumerate_gen 5;;
+
+(* }}} *)
 
